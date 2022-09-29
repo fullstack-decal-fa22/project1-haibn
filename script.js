3ffb0ab3cda3b5dbdafc87181f8d45ed268e4758
Haibin Yu Yu <haibinyuy@berkeley.edu>
2022-09-29 11:00:05 -0700

    visibility fixed

--- script.js
+++ script.js
@@ -2,67 +2,65 @@ function moveLeft() {
     let headerText = document.querySelector('#header');
     let aboutText = document.querySelectorAll('.aHead, .aBody, .wHead, .wBody');
     let contactImgs = document.querySelectorAll('.github, .linkedin, .email');
-    let btn = document.querySelector('button');
+    let btn = document.querySelector('.btn');
     console.log(aboutText)
     if (headerText.style.left === "30%") {
         headerText.style.left = "50%";
 
         // .aHead
-        aboutText[0].style.transition = "left 0.9s ease-out 100ms, opacity 0.8s ease-out 0.10s";
+        aboutText[0].style.transition = "left 0.9s ease-out 100ms, opacity 0.8s ease-out 0.10s, visibility 1s ease-in-out";
         // .aBody
-        aboutText[1].style.transition = "left 1s ease-out 100ms, opacity 0.7s ease-out 0.08s";
+        aboutText[1].style.transition = "left 1s ease-out 100ms, opacity 0.7s ease-out 0.08s, visibility 1s ease-in-out";
         // .wHead
-        aboutText[2].style.transition = "left 1.2s ease-out 100ms, opacity 0.6s ease-out 0.04s";
+        aboutText[2].style.transition = "left 1.2s ease-out 100ms, opacity 0.6s ease-out 0.04s, visibility 1s ease-in-out";
         // .wBody
-        aboutText[3].style.transition = "left 1.3s ease-out 100ms, opacity 0.5s ease-out 0.03s";
+        aboutText[3].style.transition = "left 1.3s ease-out 100ms, opacity 0.5s ease-out 0.03s, visibility 1s ease-in-out";
         // github 
-        contactImgs[0].style.transition = "left 0.9s ease-out 100ms, opacity 0.7s";
+        contactImgs[0].style.transition = "left 0.9s ease-out 100ms, opacity 0.7s, visibility 1s ease-in-out";
         // linkedin 
-        contactImgs[1].style.transition = "left 0.8s ease-out 100ms, opacity 0.6s";
+        contactImgs[1].style.transition = "left 0.8s ease-out 100ms, opacity 0.6s, visibility 1s ease-in-out";
         // email 
-        contactImgs[2].style.transition = "left 0.7s ease-out 100ms, opacity 0.5s";
+        contactImgs[2].style.transition = "left 0.7s ease-out 100ms, opacity 0.5s, visibility 1s ease-in-out";
+        
+        btn.classList.remove('btn--show');
     } else {
         headerText.style.left = "30%";
         
         // .aHead
-        aboutText[0].style.transition = "left 0.9s ease-out 100ms, opacity 1.8s";
+        aboutText[0].style.transition = "left 0.9s ease-out 100ms, opacity 1.8s, visibility 1s ease-in-out";
         // .aBody
-        aboutText[1].style.transition = "left 1s ease-out 100ms, opacity 1.9s";
+        aboutText[1].style.transition = "left 1s ease-out 100ms, opacity 1.9s, visibility 1s ease-in-out";
         // .wHead
-        aboutText[2].style.transition = "left 1.2s ease-out 100ms, opacity 2s";
+        aboutText[2].style.transition = "left 1.2s ease-out 100ms, opacity 2s, visibility 1s ease-in-out";
         // .wBody
-        aboutText[3].style.transition = "left 1.3s ease-out 100ms, opacity 2.1s";
+        aboutText[3].style.transition = "left 1.3s ease-out 100ms, opacity 2.1s, visibility 1s ease-in-out";
         // github 
-        contactImgs[0].style.transition = "left 0.7s ease-out 100ms, opacity 2.8s ease-out 0.03s";
+        contactImgs[0].style.transition = "left 0.7s ease-out 100ms, opacity 2.8s ease-out 0.03s, visibility 1s ease-in-out";
         // linkedin 
-        contactImgs[1].style.transition = "left 0.8s ease-out 100ms, opacity 2.9s ease-out 0.08s";
+        contactImgs[1].style.transition = "left 0.8s ease-out 100ms, opacity 2.9s ease-out 0.08s, visibility 1s ease-in-out";
         // email 
-        contactImgs[2].style.transition = "left 0.9s ease-out 100ms, opacity 3s ease-out 0.13s";
+        contactImgs[2].style.transition = "left 0.9s ease-out 100ms, opacity 3s ease-out 0.13s, visibility 1s ease-in-out";
         
-        btn.style.visibility = 'visible';
-        btn.style.transition = "opacity 3s";
-        btn.style.opacity = '1';
+        btn.classList.add('btn--show');
     }
 
     for (i=0; i<aboutText.length; i++) {
         if (headerText.style.left === "30%") {
             aboutText[i].style.left = "50%";
-            aboutText[i].style.opacity = "1";
+            aboutText[i].classList.add('text--show');
         } else {
             aboutText[i].style.left = "35%";
-            aboutText[i].style.opacity = "0";
+            aboutText[i].classList.remove('text--show');
         }        
     }
 
     for (i=0; i<contactImgs.length; i++) {
         if (headerText.style.left === "30%") {
             contactImgs[i].style.left = "-20.1%";
-            contactImgs[i].style.opacity = "1";
-            // contactImgs[i].style.transition = "opacity 1s"
+            contactImgs[i].classList.add('img--show');
         } else {
             contactImgs[i].style.left = "-5%";
-            contactImgs[i].style.opacity = "0";
-            // contactImgs[i].style.transition = "left 0.9s ease-out 100ms, opacity 1s ease-out 100ms";
+            contactImgs[i].classList.remove('img--show');
         }        
     }
 }
